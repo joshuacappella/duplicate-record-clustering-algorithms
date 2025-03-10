b13f9a532f4fd08d7f449b85ee92e89a030370be
bsmallyt <benjaminsmallyt@gmail.com>
2025-03-10 18:36:27 -0500

    update queries

--- Indexing/indexing.py
+++ Indexing/indexing.py
@@ -2,6 +2,9 @@ import mariadb
 import sys
 
 #Establish connection to MariaDB server
+
+# NEED TO RUN:  ssh -L 3306:localhost:3306  devel@10.5.193.178
+# This creates the connection from the server for the python script to connect to.
 try:
   conn = mariadb.connect(
     host="127.0.0.1",
@@ -14,14 +17,23 @@ except mariadb.Error as e:
 
 
 mycursor = conn.cursor()
- 
 mycursor.execute("use Kardia_DB;")
+
+
 mycursor.execute("Show tables;")
- 
 myresult = mycursor.fetchall()
  
 for x in myresult:
   print(x)
+
+print("\n")
+
+mycursor.execute("SELECT * FROM p_partner WHERE p_partner_key = \"86869\";")
+myresult = mycursor.fetchall()
+ 
+for x in myresult:
+  print(x)
+
 
 
 conn.close()
